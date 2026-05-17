0953ccbed24a330de37a28bbf51a7d3173be0145
learngermanghana <learngermanghana@gmail.com>
2026-05-17 10:41:17 +0000

    Show public link summary on account page

--- web/src/pages/CleanAccountOverview.tsx
+++ web/src/pages/CleanAccountOverview.tsx
@@ -1,9 +1,11 @@
+import PublicLinkSummaryCard from '../components/PublicLinkSummaryCard'
 import AccountOverview from './AccountOverview'
 import './CleanAccountOverview.css'
 
 export default function CleanAccountOverview() {
   return (
     <div className="clean-account-overview">
+      <PublicLinkSummaryCard />
       <AccountOverview defaultAccountTab="workspace" />
     </div>
   )
