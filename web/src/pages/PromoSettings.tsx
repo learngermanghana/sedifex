e682c4aed482aca54ba9681da0c119fa4bd52ab2
learngermanghana <learngermanghana@gmail.com>
2026-05-17 00:08:14 +0000

    Add standalone promo content page

---
+++ web/src/pages/PromoSettings.tsx
@@ -0,0 +1,5 @@
+import AccountOverview from './AccountOverview'
+
+export default function PromoSettings() {
+  return <AccountOverview headingLevel="h1" viewMode="promotions" defaultPublicPageTab="promo" />
+}
