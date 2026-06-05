6cee725774ccadceeeefffa66ba80a14598994d7
learngermanghana <learngermanghana@gmail.com>
2026-06-05 16:19:50 +0000

    Mount Ask Sedifex button in workspace layout

--- web/src/layout/ShellLayout.tsx
+++ web/src/layout/ShellLayout.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import AskSedifexAgent from '../components/AskSedifexAgent'
 import Shell from './Shell'
 
 export function ShellLayout() {
   return (
     <Shell>
       <Outlet />
+      <AskSedifexAgent enabled />
     </Shell>
   )
 }
