b9e202595a0eccb7f308978d76ff2a24b2dc445f
learngermanghana <learngermanghana@gmail.com>
2026-07-01 10:17:30 +0000

    Hide Ask Sedifex launcher

--- web/src/layout/ShellLayout.tsx
+++ web/src/layout/ShellLayout.tsx
@@ -3,11 +3,14 @@ import { Outlet } from 'react-router-dom'
 import AskSedifexAgent from '../components/AskSedifexAgent'
 import Shell from './Shell'
 
+const SHOW_ASK_SEDIFEX = false
+
 export function ShellLayout() {
   return (
     <Shell>
       <Outlet />
-      <AskSedifexAgent enabled />
+      {/* Temporarily hide Ask Sedifex until it is ready to return. */}
+      {SHOW_ASK_SEDIFEX ? <AskSedifexAgent enabled /> : null}
     </Shell>
   )
 }
